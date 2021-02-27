08bb2daf5135cc542fd6256057e6652b8feed7a3
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-02-27 13:42:10 +0100

    created all functions for "meets" expectations

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -17,7 +17,33 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const perPage = 9;
 
+function showPage (list, page){
+  const startIndex = (page * perPage) - perPage;
+  const endIndex = (page * perPage);
+  const studentList = document.querySelector('.student-list');
+  studentList.innerHTML = '';
+
+  for (let i = 0; i < list.length; i ++){
+    if (i >= startIndex && i < endIndex ){
+      let htmlString = `
+        <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+            <h3>"${list[i].name.title}, ${list[i].name.first} ${list[i].name.last}"</h3>
+            <span class="email">"${list[i].email}"</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">"Joined ${list[i].registered.date}"</span>
+          </div>
+        </li>
+        `
+      studentList.insertAdjacentHTML('beforeend', htmlString);
+    }
+  }
+  return studentList;
+}
 
 
 /*
@@ -25,6 +51,34 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination (list){
+  const amountOfPages = Math.ceil(list.length / perPage)+1;
+  const linkList = document.querySelector('.link-list');
+  linkList.innerHTML = '';
+  for (let i = 1; i < amountOfPages; i ++){
+    let htmlString = `
+      <li>
+        <button type="button">${i}</button>
+      </li>
+    `;
+    linkList.insertAdjacentHTML('beforeend', htmlString);
+  }
 
+  const firstButton = linkList.firstElementChild;
+  firstButton.className = 'active';
+
+  linkList.addEventListener('click', (e) => {
+    if (e.target.className !== 'active'){
+      let activeButton = document.querySelector('.active')
+      activeButton.className = '';
+      e.target.className = 'active';
+      let page = e.target.textContent;
+      showPage(data, page);
+    }
+  });
+  return linkList;
+}
 
 // Call functions
+showPage (data, 1);
+addPagination(data);
