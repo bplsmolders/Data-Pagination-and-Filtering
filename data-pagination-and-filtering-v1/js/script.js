dbc8b7dd3dad2b7067ec9453e8bdf6b739023cd4
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-03-01 11:46:56 +0100

    adding exceeds functionalities

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -3,19 +3,40 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+// Unerneath are all the elements created for the searchbar in the header.
+const label = document.createElement('label');
+label.for = 'search'
+label.className = 'student-search'
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+const input = document.createElement('input');
+input.type = 'text';
+input.name = 'name';
+input.placeholder = 'Search by Name';
+input.id = 'search';
+
+const searchButton = document.createElement('button');
+searchButton.type = 'submit';
+searchButton.name = 'submit';
+searchButton.value = 'submit';
+
+const image = document.createElement('img');
+image.src = 'img/icn-search.svg';
+image.alt = 'Search icon'
+
+const header = document.querySelector('header');
+header.appendChild(label);
+label.appendChild(input);
+label.appendChild(searchButton);
+searchButton.appendChild(image);
+
+//This function will create and insert/append the elements needed to display a "page" of nine students
 const perPage = 9;
-
 function showPage (list, page){
   const startIndex = (page * perPage) - perPage;
   const endIndex = (page * perPage);
   const studentList = document.querySelector('.student-list');
   studentList.innerHTML = '';
-
+  // The loop adds data in the required layout to display on the page
   for (let i = 0; i < list.length; i ++){
     if (i >= startIndex && i < endIndex ){
       let htmlString = `
@@ -37,15 +58,12 @@ function showPage (list, page){
 }
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
+//This function will create and insert/append the elements needed for the pagination buttons
 function addPagination (list){
   const amountOfPages = Math.ceil(list.length / perPage)+1;
   const linkList = document.querySelector('.link-list');
   linkList.innerHTML = '';
+  // This loop creates buttons in relation to the amount of pages.
   for (let i = 1; i < amountOfPages; i ++){
     let htmlString = `
       <li>
@@ -55,9 +73,13 @@ function addPagination (list){
     linkList.insertAdjacentHTML('beforeend', htmlString);
   }
 
+  /*
+  When there is at least 1 student being diplayed, the created buttons will have the class active applied
+  If there are now students displayed, the message 'no Results' is displayed
+  */
+  if (list.length > 1){
   const firstButton = linkList.firstElementChild;
   firstButton.className = 'active';
-
   linkList.addEventListener('click', (e) => {
     if (e.target.className !== 'active'){
       let activeButton = document.querySelector('.active')
@@ -65,50 +87,31 @@ function addPagination (list){
       e.target.className = 'active';
       let page = e.target.textContent;
       showPage(data, page);
-    }
-  });
+      }
+    });
+  } else {
+    htmlString = `
+      <h2>
+        No Results
+      </h2>
+    `;
+    linkList.insertAdjacentHTML('beforeend', htmlString);
+  }
   return linkList;
-}
+  }
 
-// Call functions
+// Calls functions
 showPage (data, 1);
 addPagination(data);
 
-// Extra search function
-const label = document.createElement('label');
-label.for = 'search'
-label.className = 'student-search'
-
-const input = document.createElement('input');
-input.type = 'text';
-input.name = 'name';
-input.placeholder = 'Search by Name';
-input.id = 'search';
-
-const searchButton = document.createElement('button');
-searchButton.type = 'submit';
-searchButton.name = 'submit';
-searchButton.value = 'submit';
-
-const image = document.createElement('image');
-image.src = 'img/icn-search.svg';
-image.alt = 'Search icon'
-
-const header = document.querySelector('header');
-header.appendChild(label);
-label.appendChild(input);
-label.appendChild(searchButton);
-header.appendChild(image);
-
+// Underneath the search filter is activated when the user hits enter or the searchbutton.
 label.addEventListener ('change', (e)=> {
   e.preventDefault();
-  let text = input.value
-  input.value ='';
-  const filteredList = [];
+  let text = input.value.toLowerCase();
+  let filteredList = [];
   for (let i = 0; i <data.length ; i++){
-    if (data[i].name.first.includes(text) || data[i].name.last.includes(text) ){
-      const filteredList = data[i];
-      console.log(filteredList);
+    if (data[i].name.first.toLowerCase().includes(text) || data[i].name.last.toLowerCase().includes(text) ){
+      filteredList = filteredList.concat(data[i]);
     }
   }
   showPage(filteredList, 1);
